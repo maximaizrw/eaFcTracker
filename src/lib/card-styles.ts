3bdeb3554dac8dfbe25afa360869d5cf0f230222
Maxi Maiz <maximaizrw@gmail.com>
2025-09-19 15:38:53 +0000

    Las cartas no se ven

--- src/lib/card-styles.ts
+++ src/lib/card-styles.ts
@@ -10,9 +10,9 @@ export const cardStyleImages: Record<CardStyle, string> = {
     'silver-common': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/4.png',
     'bronze-rare': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/8.png',
     'bronze-common': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/7.png',
-    'totw': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/11.png', // Example for TOTW, might need adjustments
-    'tots': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/13.png', // Example for TOTS
-    'toty': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/12.png', // Example for TOTY
-    'hero': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/22.png', // Example for Hero
-    'icon': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/9.png',   // Example for Icon
+    'totw': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/11.png',
+    'tots': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/13.png',
+    'toty': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/12.png',
+    'hero': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/22.png',
+    'icon': 'https://cdn3.futbin.com/content/fifa24/img/cards/tiny/9.png',
 };
